9494ce252380b4e55a4dfe2191b45df491e96bfa
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-27 19:20:57 +0100

    chore: Liste des handlers pour les programmes

--- src/controllers/programs.controller.ts
+++ src/controllers/programs.controller.ts
@@ -5,3 +5,10 @@ export const getPrograms = async (req, res) => {
 
   res.status(200).json({ data: programs });
 };
+
+export const getProgramById = async (req, res) => {};
+export const createProgram = async (req, res) => {};
+export const updateProgram = async (req, res) => {};
+export const deleteProgram = async (req, res) => {};
+export const getProgramsByUserId = async (req, res) => {};
+export const getProgramByAuthorId = async (req, res) => {};
