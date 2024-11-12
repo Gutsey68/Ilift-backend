9b5ffb3467480e8f47b83487c515a37cc06c53da
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-11-12 16:29:31 +0100

    feat: ajout d'une expiration de 1 heure au token JWT

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 export const createJWT = user => {
-  const token = jwt.sign({ id: user.id, pseudo: user.pseudo }, process.env.JWT_SECRET);
+  const token = jwt.sign({ id: user.id, pseudo: user.pseudo }, process.env.JWT_SECRET, { expiresIn: '1h' });
   return token;
 };
