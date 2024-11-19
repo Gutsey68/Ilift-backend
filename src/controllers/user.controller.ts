76d343289a02f754df29cd10839966648906179e
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-11-19 09:51:08 +0100

    refactor: Modification de la route pour récupérer les utilisateurs suggérés

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { getSuggestedUsers, getUserProfile, getUsers } from '../services/user.se
 export const getUsersHandler = async (req, res) => {
   try {
     const users = await getUsers();
+
     res.status(200).json({ message: 'Utilisateurs récupérés avec succès', data: users });
   } catch (error) {
     res.status(500).json({ error: 'Erreur Interne du Serveur' });
@@ -18,6 +19,7 @@ export const getUserProfileHandler = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'Utilisateur non trouvé' });
     }
+
     res.status(200).json({ message: 'Utilisateur récupéré avec succès', data: user });
   } catch (error) {
     res.status(500).json({ error: 'Erreur Interne du Serveur' });
@@ -33,6 +35,7 @@ export const getCurrentUserHandler = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'Utilisateur non trouvé' });
     }
+
     res.status(200).json({ message: 'Utilisateur récupéré avec succès', data: user });
   } catch (error) {
     res.status(500).json({ error: 'Erreur Interne du Serveur' });
@@ -41,7 +44,7 @@ export const getCurrentUserHandler = async (req, res) => {
 
 export const getSuggestedUsersHandler = async (req, res) => {
   try {
-    const userId = req.params.id;
+    const userId = req.user.id;
 
     const users = await getSuggestedUsers(userId);
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,8 +5,8 @@ import { protect } from '../middlewares/protect';
 const userRoutes = Router();
 
 userRoutes.get('/me', protect, getCurrentUserHandler);
+userRoutes.get('/suggested', protect, getSuggestedUsersHandler);
 userRoutes.get('/:id', protect, getUserProfileHandler);
 userRoutes.get('/', protect, getUsersHandler);
-userRoutes.get('/:id/suggested', protect, getSuggestedUsersHandler);
 
 export default userRoutes;
